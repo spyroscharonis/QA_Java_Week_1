703783f348c55debf11aa6fecb646290aadf58e6
Spyros <s.charonis@gmail.com>
2017-07-24 17:26:26 +0000

    Library changed

--- Library/src/Librarian.java
+++ Library/src/Librarian.java
@@ -1,14 +1,28 @@
+import java.util.*; 
 
-public class Librarian {
+
+public class Library {
 	
-	String name;
-	String password;
+	String address; 
+	ArrayList<item> items = new ArrayList<item>(); 
 	
-	// constructor
-	public Librarian() {
-		
-		this.name = name;
-		this.password = password; 
+	private static final String openingTime = "9am";
+	private static final String closingTime = "5pm"; 
+	
+	
+	// constructor 
+	public Library(String libAddress) {
+		libAddress = address;
 	}
+	
+	
+	public void addItem(Item item) {
+		items.add(item);
+	}
+	
+	public void removeItem(item item) {
+		items.remove(item);
+	}
+	
 	
 }
